5a69e8687fd65efa4c2c55f8cbfb8f5c10860709
Danillo Gustavo Monteiro <monteirod@brascabos.intranet>
2023-05-18 09:14:05 -0300

    adicionando o babel

---
+++ dist/src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+
+mongoose.connect("mongodb+srv://danillo324:danillo324@cluster0.kwdapfr.mongodb.net/?retryWrites=true&w=majority", {
+  useNewUrlParser: true
+}).then(() => console.log("Connection succesful")).catch(err => console.log(err));
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+app.use(require("./routes"));
+
+app.listen(process.env.PORT || 3001);
